57f2e016d8638b60cef8b22860c83c93ce41539b
Daniel Richards <danielr@werinteractive.com>
2013-04-02 14:22:06 +0100

    Partially incorporated SimpleGrid layout

--- config/projects.js
+++ config/projects.js
@@ -2,6 +2,7 @@ module.exports = [
 	{ 
 		"name" : "playr",
 		"title" : "I Am Playr",
+		"cssThumbnailClass" : "slot-0-1",
 		"description" : "I Am Playr is a popular social game, where players are given the opportunity to experience the life of a professional footballer.",
 		"company" : "We R Interactive",
 		"mainImage": "playr/main.jpg"
@@ -9,6 +10,7 @@ module.exports = [
 	{ 
 		"name" : "vizi",
 		"title" : "ViziQuest",
+		"cssThumbnailClass" : "slot-2-3",
 		"description" : "ViziQuest is a semantic browsing portal that allows users to naturally find their way through large media collections.",
 		"company" : "Deep Visuals",
 		"mainImage": "vizi/main.jpg"
@@ -16,6 +18,7 @@ module.exports = [
 	{ 
 		"name" : "splats",
 		"title" : "Splats",
+		"cssThumbnailClass" : "slot-4-5",
 		"description" : "Four tiny creatures are accidentally brought to life in a lab experiment, can you help them escape their captors?",
 		"company" : "NPR3 / Dare To Be Digital",
 		"mainImage": "splats/main.jpg"
